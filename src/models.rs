3860ef2e4e303c5029ae543c4863b15c8ce777be
Act0r1 <gizatullininsaf25@gmail.com>
2022-07-25 01:52:55 +0300

    fix struct "cannot find sql_type"

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,11 @@
-use diesel::Queryable;
+use diesel::pg::Pg;
+use diesel::types::{FromSql,ToSql};
+use serde::{Deserialize, Serialize};
+extern crate serde_json;
 use crate::schema::{chats, messages};
+use diesel::sql_types::Jsonb;
 use diesel::Insertable;
-#[derive( Queryable, PartialEq, Debug)]
+#[derive( Queryable, PartialEq, Debug, Deserialize, Serialize)]
 pub struct Chats{
    pub chat_id: i64,
    pub room: i64,
@@ -13,9 +17,7 @@ pub struct Chats{
    pub open: bool,
    pub description: String,
 }
-#[derive(Identifiable, Queryable, Associations, PartialEq, Debug)]
-#[belongs_to(Chats)]
-#[table_name = "chats"]
+#[derive(Queryable, Associations, PartialEq, Debug)]
 pub struct Messages {
     pub id: i64,
     pub author: String,
@@ -25,26 +27,48 @@ pub struct Messages {
     pub who_read: String,
 }
 
-#[derive(Debug, Insertable)]
+#[derive(Debug,FromSqlRow, Insertable, Serialize, Deserialize)]
 #[table_name="chats"]
-pub struct NewGroup<'a> {
+#[diesel(sql_type = "Jsonb")]
+pub struct NewGroup {
    pub creator: String,
    pub displayed_name: String,
    pub chat_type: String,
    pub avatar: String,
-   pub users: &'a Vec<String>,
+   pub users: Vec<String>,
    pub open: bool,
    pub description: String,
 }
 
-#[derive(Debug, Insertable)]
+#[derive(Debug, AsExpression,FromSqlRow, Insertable, Serialize, Deserialize)]
 #[table_name="chats"]
-pub struct NewChat<'a> {
+#[diesel(sql_type = "Jsonb")]
+pub struct NewChat {
    pub creator: String,
    pub displayed_name: String,
    pub chat_type: String,
    pub avatar: String,
-   pub users: &'a Vec<String>,
+   pub users: Vec<String>,
    pub open: bool,
    pub description: String,
 }
+
+
+impl FromSql<Jsonb, Pg> for NewChat {
+    fn from_sql() {}
+}
+
+
+
+
+impl ToSql<Jsonb, Pg> for NewChat {
+    fn to_sql() {}
+}
+
+
+
+
+
+
+
+
